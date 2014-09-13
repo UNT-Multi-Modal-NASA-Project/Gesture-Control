4efecf8195f11eb46c5beaeb63e0e8b96dd108d6
Tinker <tinkerofwales@gmail.com>
2014-09-13 10:53:17 -0500

    Starting from scratch, and moving incrementally. First, get data. Success in pwrsamptest0

--- pwrsamptest0.c
+++ pwrsamptest0.c
@@ -2,6 +2,8 @@
 * Experimental program for testing the RX power reading from the wireless
 * adapter. Uses the general wireless interface defined in wireless.h and
 * ioctl. Should be complied with the -lm flag
+*
+* The point of this code is to figure out how to get level information.
 */
 //selected includes
 #include <stdio.h>
@@ -13,12 +15,10 @@
 #include <linux/wireless.h>
 #include <tgmath.h>
 
-#define READ 0
-#define UNREAD 1
-
 int u8_to_dbm(const int power);
 double dbm2mw(const double in);
-
+char scale(double*);
+double mavg(double, double, double);
 void get_samples(char * iwname, int max_samples);
 static void iw_getstat(struct iw_statistics *, char *);//actually reads the value
 
@@ -48,7 +48,8 @@ void get_samples(char* ifname,int samples)
 {
 	struct iw_statistics reading;
 	int level, dbm, noise_lvl, noise_dbm;
-	double power,noise_pwr;
+	double power,noise_pwr,avg_pwr;
+	char unit, noise_unt;
 	int i=0;
 
 	printf("Reading levels:\n");
@@ -62,11 +63,17 @@ void get_samples(char* ifname,int samples)
 		noise_lvl=reading.qual.noise;
 		noise_dbm=u8_to_dbm(noise_lvl);
 		noise_pwr=dbm2mw(noise_dbm);
-		printf("%d\t%d(dbm)\t%.2f(mW)\t%d\t%d(dbm)\t%.2f(mW)\n", level,dbm,power, noise_lvl, noise_dbm, noise_pwr);
+		unit=scale(&power);
+		noise_unt=scale(&noise_pwr);
+		printf("%d\t%d(dbm)\t%.2f(%cW)\t%d\t%d(dbm)\t%.2f(%cW)\n", level,dbm,power,unit, noise_lvl, noise_dbm, noise_pwr,noise_unt);
 		i++;
+		avg_pwr=mavg(avg_pwr,power,i);
 	}
+	printf("%.2f(%cW)\n",avg_pwr,unit);
 }
-
+double mavg(double avg,double newread,double num){
+	return (avg*(num-1)+newread)/num;
+}
 static void iw_getstat(struct iw_statistics *stats, char* iwname){
 	int sockfd;
 	struct iwreq req;
@@ -101,5 +108,20 @@ int u8_to_dbm(const int power)
 double dbm2mw(const double in)
 {
         return pow(10.0, in / 10.0);
+}
+char scale(double* in)
+{
+        if (*in < 0.00000001) {
+		(*in)*=1e9;
+		return 'p';
+        } else if (*in < 0.00001) {
+		(*in)*=1e6;
+		return 'n';
+        } else if (*in < 0.01) {
+		(*in)*=1e3;
+		return 'u';
+        } else {
+		return 'm';
+        }
 }
 
